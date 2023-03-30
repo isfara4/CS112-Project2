ebcfd53de9994d0b22389e632a697021f03ad57e
isfara4 <129343109+isfara4@users.noreply.github.com>
2023-03-29 22:44:24 -0700

    updated Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ package for_GameProject2;
 
 public abstract class Game {
 
-	public abstract String rules();
+	public abstract void rules();
     public abstract void setUp();
     public abstract boolean goodPlayerInput(String guess);
     public abstract void checkWinOrLose();
