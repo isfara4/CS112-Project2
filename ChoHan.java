8daba37435b85fcc5b8cfef4d9b08dee92fb460b
isfara4 <129343109+isfara4@users.noreply.github.com>
2023-03-29 22:43:57 -0700

    finished ChoHan

--- ChoHan.java
+++ ChoHan.java
@@ -7,19 +7,17 @@ public class ChoHan extends Game {
 
 	Random random = new Random();
 	int money = 2;
-	int sum; 
-	int dice1;
-	int dice2;
-	String answer;
+	int sum = 0; 
+	int dice1 = 0;
+	int dice2 = 0;
+	String answer = "";
 	String evenOrOdd = "";
 
 	
 	
 	public ChoHan() {
 		super();
-		dice1 = random.nextInt(5) + 1;
-		dice2 = random.nextInt(5) + 1;
-		sum = dice1 + dice2;
+
         if(sum % 2 == 0) {
             answer = "CHO";
         }else
@@ -27,18 +25,20 @@ public class ChoHan extends Game {
 	}
 	
 	@Override
-	public String rules() {
-		// TODO Auto-generated method stub
-		return "Cho-Han is a traditional Japanese guessing game.\n"
+	public void rules() {
+		System.out.println( "Cho-Han is a traditional Japanese guessing game.\n"
 				+ "A dealer rolls two 6-sided die, and you must guess\n"
-				+ "if thier sum is CHO (even) or HAN (odd).\n"
+				+ "if their sum is CHO (even) or HAN (odd).\n"
 				+ "You start with $2. A wrong answer loses you $1 and \n"
 				+ "a right answer wins $1. You must stop playing at $0\n"
-				+ "Good luck!\n\n";
+				+ "Good luck!\n\n");
 	}
 
 	@Override
 	public void setUp() {
+		dice1 = random.nextInt(5) + 1;
+		dice2 = random.nextInt(5) + 1;
+		sum = dice1 + dice2;
 		System.out.println("The dealer swirls the cup and you hear the dice rattle\n"
 							+ "The dealer slams the cup on the floor, covering the dice\n"
 							+ "and asks for your bet.\n\n    CHO (even) or HAN (odd)");
@@ -49,20 +49,20 @@ public class ChoHan extends Game {
 	public boolean goodPlayerInput(String guess) {
 
 		if(guess.toUpperCase().equals("CHO") || guess.toUpperCase().equals("HAN")){
-			guess = evenOrOdd;
+			evenOrOdd = guess;
 			return true;
-		}else if(guess.equalsIgnoreCase("quit")) {
-		return true;
-		}
-		return false;	// edit
+		}else
+			return false;
 	}
+	// edit
+	
 
 	@Override
 	public void checkWinOrLose() {
 		System.out.println("The dealer lifts his cup to reveal:\n"
 				+ dice1 + " - " + dice2 + "\n"
 				+ "The correct answer is: " + answer);
-		if(evenOrOdd.toUpperCase().equals(answer)) {
+		if(evenOrOdd.equalsIgnoreCase(answer)) {
 			System.out.println("You won!");
 			money++;
 		}else {
